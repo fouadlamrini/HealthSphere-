003bc8a3245b41ca0f40253c0656b5e926580801
fouad lmrini <fouad11.lamrini@gmail.com>
2026-02-26 10:01:01 +0000

    new branch feature-navigation

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "@/screens/HomeScreen";
+import AddWorkoutScreen from "@/screens/AddWorkoutScreen";
+import WorkoutDetailsScreen from "@/screens/WorkoutDetailsScreen";
+
+const Stack = createNativeStackNavigator();
+export default function index() {
+  return (
+    <Stack.Navigator initialRouteName="Add">
+      <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+      <Stack.Screen name="Add" component={AddWorkoutScreen}></Stack.Screen>
+      <Stack.Screen
+        name="Detail"
+        component={WorkoutDetailsScreen}
+      ></Stack.Screen>
+    </Stack.Navigator>
+  );
+}
